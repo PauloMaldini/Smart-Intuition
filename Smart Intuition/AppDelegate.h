8cc431b0198190ffe42f0fd24277d498760a20a5
Kostya <user@Kostyas-MacBook.local>
2012-02-17 17:35:29 +0900

    Initial Commit

---
+++ Smart Intuition/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Smart Intuition
+//
+//  Created by Konstantin Tsymbalist on 17.02.12.
+//  Copyright (c) 2012 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Smart Intuition/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Smart Intuition
+//
+//  Created by Konstantin Tsymbalist on 17.02.12.
+//  Copyright (c) 2012 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
